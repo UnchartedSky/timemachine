6f90e8676581dede30c0e03cea6d35ee298eb1d3
Jaehoon Choi <plaintext@andromedarabbit.net>
2016-11-09 00:01:46 +0900

    It will pass the tests

--- test/integration/test-facebook-api.js
+++ test/integration/test-facebook-api.js
@@ -1,9 +1,24 @@
 var supertest = require('supertest'),
-    app = require('../../app');
+    assert = require('assert'),
+    facebook = require('../../lib/facebook'),
+    app = require('../../app'),
+    util = require('util');
 
-exports.list_posts = function(done){
-    supertest(app)
-        .get('/listPosts?accessToken=EAAZArRbkd5G8BABfkqwVZBEHh4FNUZCVxhbljlglXez2F6yYXZAaZBbpYRdkQO0BRz9sTfO6svbZA2QYpS25UxO047YEFOOmGtWUtKhtNFkYhnEDvSm1oLmFGZBWhURKxqeukIf1tYQ1728mr8TyGeoZCrCn6ZBHV8qSTZBtmeloWSb5bxTNFcw2EZA')
-        .expect(200)
-        .end(done);
+exports.list_posts = function (done) {
+    this.timeout(10000);
+
+    FBTestUsers = require('fb-test-users');
+    fbTestUsers = new FBTestUsers({appID: '1806797062923375', secret: '25d08987cbc8b01f44ec7a4a0adf47f8'});
+
+    fbTestUsers.create({installed: true, permissions: 'user_posts'}, function (error, result) {
+        assert.ifError(error);
+
+        const limit = 2;
+
+        supertest(app)
+            .get(util.format('/listPosts?accessToken=%s&limit=%d', result.access_token, limit))
+            .expect(200)
+            .end(done);
+    });
+
 };
